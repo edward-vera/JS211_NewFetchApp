410ab9d975f27b1fb2a5d3b7628d2c736bbb1eb7
edward-vera <103536879+edward-vera@users.noreply.github.com>
2022-08-14 20:23:08 -0500

    initial commit

--- main.js
+++ main.js
@@ -1,7 +1,32 @@
+const fetch = require('node-fetch');
+const { getHeapSpaceStatistics } = require('v8');
+
+const url = 'api link needed here'
+
+window.onload = function() {
+
+    getSomething();
+}
+
 require('dotenv').config();
 
 // console.log(process.env)
 
 const api_key = process.env.SECRET_API_KEY
 
+const api_public = process.env.SECRET_PUBLIC_KEY
+
+const ts = new Date().getTime
+
 console.log(api_key)
+console.log(api_public)
+
+window.onload = function() {
+
+    getSomething();
+}
+
+const fetchStuff = () => {
+    // do the fetch
+}
+console.log(fetchStuff)
